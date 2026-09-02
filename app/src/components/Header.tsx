990a39befde6aa40d6c64ae6c63ce41e0f0c79e0
manas jha <manasjha384@gmail.com>
2026-09-02 22:23:35 +0530

    first commit

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div>
+            <h1 className="text-center text-5xl text-white font-extrabold">
+                Hello Header
+            </h1>
+        </div>
+    )
+}
