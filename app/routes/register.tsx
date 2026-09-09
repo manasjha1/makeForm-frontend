57d60a75f94f3ae72acfa00c89479ff831e70e4c
manas jha <manasjha384@gmail.com>
2026-09-09 16:19:16 +0530

    feat: update Login and Register components; enhance layout, add Google sign-in button, and improve form styling

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,8 +1,8 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Controller, useForm } from "react-hook-form"
-import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
 
-import { Button } from "~/src/components/ui/button"
+import { Button } from "~/src/components/ui/button";
 import {
     Card,
     CardContent,
@@ -10,7 +10,7 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "~/src/components/ui/card"
+} from "~/src/components/ui/card";
 import {
     Field,
     FieldDescription,
@@ -19,21 +19,29 @@ import {
     FieldLabel,
     FieldLegend,
     FieldSet,
-} from "~/src/components/ui/field"
-import { Input } from "~/src/components/ui/input"
+} from "~/src/components/ui/field";
+import { Input } from "~/src/components/ui/input";
 import {
     InputGroup,
     InputGroupAddon,
     InputGroupInput,
     InputGroupText,
     InputGroupTextarea,
-} from "~/src/components/ui/input-group"
-import makeForm_png from "~/assests/makeForm_login.png"
-import toast, { Toaster } from 'react-hot-toast';
-import Header from "~/src/components/Header"
-import type { SetStateAction } from "react"
-import { LayoutFreeform, Lock, LucideForm, Mail, User } from "lucide-react"
-import { Link } from "react-router"
+} from "~/src/components/ui/input-group";
+import makeForm_png from "~/assests/makeForm_login.png";
+import toast, { Toaster } from "react-hot-toast";
+import Header from "~/src/components/Header";
+import type { SetStateAction } from "react";
+import {
+    LayoutFreeform,
+    Lock,
+    LucideForm,
+    Mail,
+    Minus,
+    User,
+} from "lucide-react";
+import { Link } from "react-router";
+import { FaGoogle, FaLine } from "react-icons/fa";
 
 const formSchema = z.object({
     name: z
@@ -48,7 +56,7 @@ const formSchema = z.object({
         .string()
         .min(8, "password must be at least 8 characters.")
         .max(50, "password must be at most 50 characters."),
-})
+});
 
 interface formProps {
     viewPage: toggleBtn;
@@ -63,19 +71,20 @@ export default function Register({ viewPage, setViewPage }: formProps) {
             email: "",
             password: "",
         },
-    })
+    });
     function onSubmit(data: z.infer<typeof formSchema>) {
-        toast.success("You submitted the following details")
+        toast.success("You submitted the following details");
         console.log("You submitted the following details", data);
-
     }
-
 
     return (
         <div>
-            <Header viewPage={""} setViewPage={function (value: SetStateAction<toggleBtn>): void {
-                throw new Error("Function not implemented.")
-            }} />
+            <Header
+                viewPage={""}
+                setViewPage={function (value: SetStateAction<toggleBtn>): void {
+                    throw new Error("Function not implemented.");
+                }}
+            />
             <div className="grid gap-6">
                 {/* logo section */}
                 <img
@@ -86,107 +95,129 @@ export default function Register({ viewPage, setViewPage }: formProps) {
                 <section className="p-2 m-2">
                     <Card className="w-full mx-auto sm:max-w-md shadow-lg">
                         <CardContent>
-                            <div className="flex items-center justify-center w-full m-auto bg-[#f9f6f0] p-1 mb-4 rounded-sm border border-[#E2E8E4]">
-                                <Link to="/sign-in">
-                                    <Button
-                                        onClick={() => setViewPage("login")}
-                                        className={`text-[12px] font-medium transition-all px-8 md:px-17 rounded-sm ${viewPage === "login" ? "bg-white hover:bg-white text-emerald-700 shadow" : "bg-transparent hover:bg-transparent text-gray-500 hover:text-black/80"}`}
-                                    >
-                                        Sign In
-                                    </Button>
-                                </Link>
-                                <Link to="/create-account">
-                                    <Button
-                                        onClick={() => setViewPage("register")}
-                                        className={`text-[12px] font-medium transition-all px-6 md:px-17 w-full rounded-sm ${viewPage !== "register" ? "bg-white hover:bg-white text-emerald-700 shadow" : "bg-transparent hover:bg-transparent text-gray-500 hover:text-black/80"}`}
-                                    >
-                                        Create Account
-                                    </Button>
-                                </Link>
-                            </div>
-                            <form id="register-form" onSubmit={form.handleSubmit(onSubmit)}>
-                                <FieldGroup>
-                                    <Controller
-                                        name="name"
-                                        control={form.control}
-                                        render={({ field, fieldState }) => (
-                                            <Field data-invalid={fieldState.invalid}>
-                                                <FieldLabel htmlFor="register-form-name">
-                                                    Full Name
-                                                </FieldLabel>
-                                                <InputGroup className="rounded-sm">
-                                                    <InputGroupInput  {...field}
-                                                        id="register-form-name"
-                                                        aria-invalid={fieldState.invalid}
-                                                        placeholder="enter your name"
-                                                        autoComplete="off" />
-                                                    <InputGroupAddon>
-                                                        <User />
-                                                    </InputGroupAddon>
-                                                </InputGroup>
-                                                {fieldState.invalid && (
-                                                    <FieldError errors={[fieldState.error]} />
-                                                )}
-                                            </Field>
-                                        )}
-                                    />
-                                    <Controller
-                                        name="email"
-                                        control={form.control}
-                                        render={({ field, fieldState }) => (
-                                            <Field data-invalid={fieldState.invalid}>
-                                                <FieldLabel htmlFor="register-form-email">
-                                                    Email Address
-                                                </FieldLabel>
-                                                <InputGroup className="rounded-sm">
-                                                    <InputGroupInput  {...field}
-                                                        id="login-form-email"
-                                                        aria-invalid={fieldState.invalid}
-                                                        placeholder="enter your email"
-                                                        autoComplete="off" />
-                                                    <InputGroupAddon>
-                                                        <Mail />
-                                                    </InputGroupAddon>
-                                                </InputGroup>
-                                                {fieldState.invalid && (
-                                                    <FieldError errors={[fieldState.error]} />
-                                                )}
-                                            </Field>
-                                        )}
-                                    />
-                                    <Controller
-                                        name="password"
-                                        control={form.control}
-                                        render={({ field, fieldState }) => (
-                                            <Field data-invalid={fieldState.invalid}>
-                                                <FieldLabel htmlFor="register-form-password">
-                                                    Password
-                                                </FieldLabel>
-                                                <InputGroup className="rounded-sm">
-                                                    <InputGroupInput {...field}
-                                                        id="login-form-password"
-                                                        aria-invalid={fieldState.invalid}
-                                                        placeholder="••••••••"
-                                                        autoComplete="off" />
-                                                    <InputGroupAddon>
-                                                        <Lock />
-                                                    </InputGroupAddon>
-                                                </InputGroup>
-                                                {fieldState.invalid && (
-                                                    <FieldError errors={[fieldState.error]} />
-                                                )}
-                                            </Field>
-                                        )}
-                                    />
-                                </FieldGroup>
-                            </form>
+                            <section className="mb-5">
+                                <div className="flex items-center justify-center w-full m-auto bg-[#f9f6f0] p-1 mb-4 rounded-sm border border-[#E2E8E4]">
+                                    <Link to="/sign-in">
+                                        <Button
+                                            onClick={() => setViewPage("login")}
+                                            className={`text-[12px] font-medium transition-all px-8 md:px-17 rounded-sm ${viewPage === "login" ? "bg-white hover:bg-white text-emerald-700 shadow" : "bg-transparent hover:bg-transparent text-gray-500 hover:text-black/80"}`}
+                                        >
+                                            Sign In
+                                        </Button>
+                                    </Link>
+                                    <Link to="/create-account">
+                                        <Button
+                                            onClick={() => setViewPage("register")}
+                                            className={`text-[12px] font-medium transition-all px-6 md:px-17 w-full rounded-sm ${viewPage !== "register" ? "bg-white hover:bg-white text-emerald-700 shadow" : "bg-transparent hover:bg-transparent text-gray-500 hover:text-black/80"}`}
+                                        >
+                                            Create Account
+                                        </Button>
+                                    </Link>
+                                </div>
+                                <Button className="bg-white text-black text-sm font-medium hover:bg-white/20 border border-gray-300 w-full rounded-sm flex items-center justify-center gap-2">
+                                    <FaGoogle /> Continue with Google
+                                </Button>
+                            </section>
+                            <div className="relative my-6 text-center">
+                                <div className="absolute inset-0 flex items-center">
+                                    <div className="w-full border-t border-[#E2E8E4]" />
+                                </div>
+                                <span className="relative bg-white px-3 text-[11px] font-semibold text-[#6B7872] uppercase">
+                                    Or with work email
+                                </span>
+                            </div>{" "}
+                            <section>
+                                <form id="register-form" onSubmit={form.handleSubmit(onSubmit)}>
+                                    <FieldGroup>
+                                        <Controller
+                                            name="name"
+                                            control={form.control}
+                                            render={({ field, fieldState }) => (
+                                                <Field data-invalid={fieldState.invalid}>
+                                                    <FieldLabel htmlFor="register-form-name">
+                                                        Full Name
+                                                    </FieldLabel>
+                                                    <InputGroup className="rounded-sm">
+                                                        <InputGroupInput
+                                                            {...field}
+                                                            id="register-form-name"
+                                                            aria-invalid={fieldState.invalid}
+                                                            placeholder="enter your name"
+                                                            autoComplete="off"
+                                                        />
+                                                        <InputGroupAddon>
+                                                            <User />
+                                                        </InputGroupAddon>
+                                                    </InputGroup>
+                                                    {fieldState.invalid && (
+                                                        <FieldError errors={[fieldState.error]} />
+                                                    )}
+                                                </Field>
+                                            )}
+                                        />
+                                        <Controller
+                                            name="email"
+                                            control={form.control}
+                                            render={({ field, fieldState }) => (
+                                                <Field data-invalid={fieldState.invalid}>
+                                                    <FieldLabel htmlFor="register-form-email">
+                                                        Email Address
+                                                    </FieldLabel>
+                                                    <InputGroup className="rounded-sm">
+                                                        <InputGroupInput
+                                                            {...field}
+                                                            id="login-form-email"
+                                                            aria-invalid={fieldState.invalid}
+                                                            placeholder="enter your email"
+                                                            autoComplete="off"
+                                                        />
+                                                        <InputGroupAddon>
+                                                            <Mail />
+                                                        </InputGroupAddon>
+                                                    </InputGroup>
+                                                    {fieldState.invalid && (
+                                                        <FieldError errors={[fieldState.error]} />
+                                                    )}
+                                                </Field>
+                                            )}
+                                        />
+                                        <Controller
+                                            name="password"
+                                            control={form.control}
+                                            render={({ field, fieldState }) => (
+                                                <Field data-invalid={fieldState.invalid}>
+                                                    <FieldLabel htmlFor="register-form-password">
+                                                        Password
+                                                    </FieldLabel>
+                                                    <InputGroup className="rounded-sm">
+                                                        <InputGroupInput
+                                                            {...field}
+                                                            id="login-form-password"
+                                                            aria-invalid={fieldState.invalid}
+                                                            placeholder="••••••••"
+                                                            autoComplete="off"
+                                                        />
+                                                        <InputGroupAddon>
+                                                            <Lock />
+                                                        </InputGroupAddon>
+                                                    </InputGroup>
+                                                    {fieldState.invalid && (
+                                                        <FieldError errors={[fieldState.error]} />
+                                                    )}
+                                                </Field>
+                                            )}
+                                        />
+                                    </FieldGroup>
+                                </form>
+                            </section>
                         </CardContent>
                         <CardFooter>
-                            <Field className="grid items-center mx-auto" orientation="horizontal">
-                                <Button className="rounded-sm border border-emerald-700 text-emerald-700 hover:bg-gray-500/5 hover:text-emerald-700" type="button" variant="outline" onClick={() => form.reset()}>
-                                    Reset
-                                </Button>
-                                <Button className="rounded-sm bg-emerald-700 text-white hover:bg-emerald-800" type="submit" form="register-form">
+                            <Field orientation="horizontal">
+                                <Button
+                                    className="rounded-sm w-full bg-emerald-700 text-white hover:bg-emerald-800"
+                                    type="submit"
+                                    form="register-form"
+                                >
                                     Submit
                                 </Button>
                             </Field>
@@ -195,5 +226,5 @@ export default function Register({ viewPage, setViewPage }: formProps) {
                 </section>
             </div>
         </div>
-    )
+    );
 }
