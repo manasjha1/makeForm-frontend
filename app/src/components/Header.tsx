8b2e3cdd499ec5c3bab36691315644bdab32fa29
manas jha <manasjha384@gmail.com>
2026-09-09 15:18:19 +0530

    feat: enhance Header component; improve layout, add user icon tooltip, and adjust button styles

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -4,6 +4,7 @@ import {
     LayoutFreeform,
     LucideForm,
     RotateCcw,
+    User,
     UserRound,
 } from "lucide-react";
 import makeForm_logo from "~/assests/makeForm_logo.png";
@@ -26,8 +27,8 @@ export default function Header({ viewPage, setViewPage }: formProps) {
     const [isPageOpen, setIsPageOpen] = useState(false);
     return (
         <div>
-            <header className="w-full p-2 bg-white fixed z-20 backdrop:blur-lg shadow-lg/20">
-                <nav className="w-full md:w-[90%] h-full flex items-center justify-between m-auto gap-3">
+            <header className="w-full fixed z-20 p-2 backdrop-blur-2xl shadow-sm/20">
+                <nav className="w-full md:w-[90%] h-full flex items-center justify-between mx-auto gap-3">
                     <div className="flex items-center justify-evenly gap-6">
                         {/* logo */}
                         <Link to="/">
@@ -37,7 +38,7 @@ export default function Header({ viewPage, setViewPage }: formProps) {
                                 alt="makeForm-logo"
                             />
                         </Link>
-                        <span className="text-gray-200 text-2xl">|</span>
+                        <span className="text-gray-200 text-2xl hidden lg:flex">|</span>
                         <div className="hidden lg:flex items-center bg-[#f9f6f0] p-1 rounded-sm border border-[#E2E8E4]">
                             <Link to="/">
                                 <Button
@@ -77,16 +78,24 @@ export default function Header({ viewPage, setViewPage }: formProps) {
                                     <p>Reset to form template</p>
                                 </TooltipContent>
                             </Tooltip>
+                            <Tooltip>
+                                <TooltipTrigger className="hover:bg-gray-50 border border-[#E2E8E4] p-1 rounded-sm flex lg:hidden">
+                                    <User className="size-5 text-gray-600" />
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Create account</p>
+                                </TooltipContent>
+                            </Tooltip>
                         </TooltipProvider>
-                        <div className="hidden lg:flex items-center">
+                        <div className="hidden lg:flex items-center gap-2">
                             <Link to="/sign-in">
-                                <Button className="bg-white hover:bg-gray-50 text-black font-medium rounded-sm border border-gray-400/50">
+                                <Button className="bg-white hover:bg-gray-50 text-black text-xs font-medium rounded-sm border border-gray-400/50">
                                     Sign In
                                 </Button>
                             </Link>
                             <Link to="/create-account">
-                                <Button className="bg-emerald-700 hover:bg-white text-sm font-medium rounded-sm hover:text-black border border-gray-400/50">
-                                    <UserRound /> Sign Up
+                                <Button className="bg-emerald-700 hover:bg-white text-xs font-medium rounded-sm hover:text-emerald-700 hover:border-emerald-700 border border-gray-400/50">
+                                    <User /> Sign Up
                                 </Button>
                             </Link>
                         </div>
