1dd97364bd53aeccb72ed90e0061f18ae6fcd738
manas jha <manasjha384@gmail.com>
2026-09-15 22:02:41 +0530

    feat: add OTP field to registration form and update related types

--- app/src/config/apiBook.ts
+++ app/src/config/apiBook.ts
@@ -3,7 +3,7 @@ const AUTH_BASEURL = import.meta.env.VITE_API_BASE_URL;
 const API_URLS = {
   AUTH: {
     REGISTER_EMAIL: `${AUTH_BASEURL}/auth/register`,
-    VERIFY_OTP: `${AUTH_BASEURL}/auth/verify-email`,
+    // VERIFY_OTP: `${AUTH_BASEURL}/auth/verify-email`,
     RESEND_OTP: `${AUTH_BASEURL}/auth/resend-verification`,
     LOGIN: `${AUTH_BASEURL}/auth/login`,
   },
--- app/types/authTypes.ts
+++ app/types/authTypes.ts
@@ -2,6 +2,7 @@ type registerType = {
   name: string;
   email: string;
   password: string;
+  otp: string;
 };
 
 type verify_otpType = {
