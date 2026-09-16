0207eb771af3c482be1a7446af9a1d9f25973087
manas jha <manasjha384@gmail.com>
2026-09-16 15:25:57 +0530

    refactor: update Header component structure and styling; modify TooltipArrow class

--- app/src/components/Headers.tsx
+++ app/src/components/Headers.tsx
@@ -1,56 +1,80 @@
-import { Eye, Home, LayoutFreeform, LucideForm, RotateCcw, UserRound } from "lucide-react";
+import {
+    Eye,
+    Home,
+    LayoutFreeform,
+    LucideForm,
+    RotateCcw,
+    UserRound,
+} from "lucide-react";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
-interface FormProps {
+interface formProps {
     viewPage: toggleBtn;
     setViewPage: React.Dispatch<React.SetStateAction<toggleBtn>>;
 }
 
-export default function Header({ viewPage, setViewPage }: FormProps) {
+export default function Header({ viewPage, setViewPage }: formProps) {
+    const [isPageOpen, setIsPageOpen] = useState(false);
     return (
-        <header className="w-full p-2 bg-white backdrop:blur-lg shadow-lg/20">
-            <nav className="w-full md:w-[90%] h-full flex items-center justify-between m-auto gap-3">
-                <div className="flex items-center justify-evenly gap-6">
-                    <div className="flex items-center gap-2">
-                        <div className="bg-emerald-700 rounded-lg inset-shadow-2xs p-2">
-                            <LucideForm className="size-6 text-white" />
+        <div>
+            <header className="w-full p-2 backdrop:blur-lg shadow-lg/20">
+                <nav className="w-full md:w-[90%] h-full flex items-center justify-between m-auto gap-3">
+                    <div className="flex items-center justify-evenly gap-6">
+                        <div className="flex items-center gap-2">
+                            <div className="bg-emerald-700 rounded-lg inset-shadow-2xs p-2">
+                                <LucideForm className="size-6 text-white" />
+                            </div>
+                            <div className="flex-col items-baseline">
+                                <h4 className="text-black text-left text-xl font-bold">
+                                    make
+                                    <span className="text-left text-emerald-700 text-xl font-bold">
+                                        Form
+                                    </span>
+                                </h4>
+                                <p className="text-left text-[10px] text-gray-700 font-normal uppercase">
+                                    visual form architect
+                                </p>
+                            </div>
                         </div>
-                        <div className="flex-col items-baseline">
-                            <h4 className="text-black text-left text-xl font-bold">
-                                make<span className="text-emerald-700">Form</span>
-                            </h4>
-                            <p className="text-left text-[10px] text-gray-700 font-normal uppercase">
-                                visual form architect
-                            </p>
-                        </div>
-                    </div>
-                    <span className="text-gray-200 text-2xl">|</span>
-                    <div className="bg-[#FAF9F6] flex items-center gap-2 rounded-sm border border-gray-300 py-1 px-2">
+                        <span className="text-gray-200 text-2xl">|</span>
                         <div className="hidden md:flex items-center bg-[#FAF9F6] p-1 rounded-sm border border-[#E2E8E4]">
-                            <Button onClick={() => setViewPage("Home")} className="text-[12px] font-normal rounded-sm">
-                                <Home className="size-3" /> Home
+                            {/* is home ? text-green : text-black */}
+                            <Button
+                                onClick={() => setViewPage("Home")}
+                                className={`text-[12px] font-normal rounded-sm ${viewPage === "Home" ? "bg-white hover:bg-white text-emerald-700 shadow" : "bg-transparent hover:bg-transparent text-gray-500 hover:text-black/80"}`}
+                            >
+                                <Home className="size-3 group-hover:text-emerald-700" /> Home
                             </Button>
-                            <Button onClick={() => setViewPage("FormBuilder")} className="text-[12px] font-normal rounded-sm">
-                                <LayoutFreeform className="size-3" /> Form Builder
+                            <Button
+                                onClick={() => setViewPage("FormBuilder")}
+                                className={`text-[12px] font-normal rounded-sm ${viewPage === "FormBuilder" ? "bg-white hover:bg-white text-emerald-700 shadow" : "bg-transparent hover:bg-transparent text-gray-500 hover:text-black/80"}`}
+                            >
+                                <LayoutFreeform className="size-3 group-hover:text-emerald-700" />{" "}
+                                Form Builder
                             </Button>
-                            <Button onClick={() => setViewPage("LivePreview")} className="text-[12px] font-normal rounded-sm">
-                                <Eye className="size-3" /> Live Preview
+                            <Button
+                                onClick={() => setViewPage("LivePreview")}
+                                className={`text-[12px] font-normal rounded-sm ${viewPage === "LivePreview" ? "bg-white hover:bg-white text-emerald-700 shadow" : "bg-transparent hover:bg-transparent text-gray-500 hover:text-black/80"}`}
+                            >
+                                <Eye className="size-3 group-hover:text-emerald-700" /> Live
+                                Preview
                             </Button>
                         </div>
                     </div>
-                </div>
-                <div className="flex items-center justify-between gap-3">
-                    <Button className="bg-white hover:bg-gray-50 rounded-sm border border-gray-400/50">
-                        <RotateCcw className="text-gray-500 size-5" />
-                    </Button>
-                    <Button className="bg-white hover:bg-gray-50 text-black font-medium rounded-sm border border-gray-400/50">
-                        Sign In
-                    </Button>
-                    <Button className="bg-emerald-700 hover:bg-white text-sm font-medium rounded-sm hover:text-black border border-gray-400/50">
-                        <UserRound /> Sign Up
-                    </Button>
-                </div>
-            </nav>
-        </header>
+                    <div className="flex items-center justify-between gap-3">
+                        <Button className="bg-white hover:bg-gray-50 rounded-sm border border-gray-400/50">
+                            <RotateCcw className="text-gray-500 size-5" />
+                        </Button>
+                        <Button className="bg-white hover:bg-gray-50 text-black font-medium rounded-sm border border-gray-400/50">
+                            Sign In
+                        </Button>
+                        <Button className="bg-emerald-700 hover:bg-white text-sm font-medium rounded-sm hover:text-black border border-gray-400/50">
+                            <UserRound /> Sign Up
+                        </Button>
+                    </div>
+                </nav>
+            </header>
+        </div>
     );
 }
